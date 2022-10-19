7c9c25455c1934ff890ac98797f931d7c22f26aa
piyag27 <102835841+piyag27@users.noreply.github.com>
2022-10-18 21:09:30 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,98 @@
+prediction_1 = ""
+prediction_2 = ""
+
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/4RcRMKtpT/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded');
+}
+
+function speak(){
+    var synth= window.speechSynthesis;
+    speak_data_1= "The first prediction is " + prediction_1;
+    speak_data_2= "The second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else{
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        document.getElementById("result_gesture_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label== "Peace-sign")
+        {
+            document.getElementById("update_gesture").innerHTML = "&#9996;";
+        }
+        if(results[0].label== "Thumbs Up")
+        {
+            document.getElementById("update_gesture").innerHTML = "&#128077;";
+        }
+        if(results[0].label== "Thumbs Down")
+        {
+            document.getElementById("update_gesture").innerHTML = "&#128078;";
+        }
+        if(results[0].label== "OK")
+        {
+            document.getElementById("update_gesture").innerHTML = "&#128076;";
+        }
+        if(results[0].label== "Call Me")
+        {
+            document.getElementById("update_gesture").innerHTML = "&#129305;";
+        }
+        
+
+        if(results[1].label== "Peace-sign")
+        {
+            document.getElementById("update_gesture2").innerHTML = "&#9996;";
+        }
+        if(results[1].label== "Thumbs Up")
+        {
+            document.getElementById("update_gesture2").innerHTML = "&#128077;";
+        }
+        if(results[1].label== "Thumbs Down")
+        {
+            document.getElementById("update_gesture2").innerHTML = "&#128078;";
+        }
+        if(results[1].label== "OK")
+        {
+            document.getElementById("update_gesture2").innerHTML = "&#128076;";
+        }
+        if(results[1].label== "Call Me")
+        {
+            document.getElementById("update_gesture2").innerHTML = "&#129305;";
+        }
+    }
+}
